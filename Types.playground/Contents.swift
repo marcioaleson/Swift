a442048d806ca7fe238829a771f9de6353ae18e0
marcioaleson <marcioaleson@gmail.com>
2017-01-10 03:22:28 -0300

    Types in Swift.

---
+++ Types.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+import Darwin
+
+//integer types
+let minValue = UInt8.min //min value of integer for 8 bit form.
+print(minValue)
+let maxValue = UInt8.max //max value of integer for 8 bit form.
+print(maxValue)
+let minValue2 = UInt16.min
+print(minValue2)
+let maxValue2 = UInt16.max
+print(maxValue2)
+let minValue3 = UInt32.min
+print(minValue3)
+let maxValue3 = UInt32.max
+print(maxValue3)
+let minValue4 = UInt64.min
+print(minValue4)
+let maxValue4 = UInt64.max
+print(maxValue4)
+let minFloat = FLT_MIN
+print(minFloat)
+let maxFloat = FLT_MAX
+print(maxFloat)
+let minDouble = DBL_MIN
+print(minDouble)
+let maxDouble = DBL_MAX
+print(maxDouble)
