6d3987eaa1c5791f4e1df2c09b840456a90b629b
marcioaleson <marcioaleson@gmail.com>
2017-01-10 02:40:51 -0300

    Printing in Swift.

--- NamingConstantsVariables.playground/Contents.swift
+++ NamingConstantsVariables.playground/Contents.swift
@@ -5,5 +5,12 @@ import Cocoa
 let π = 3.14159
 let 你好 = "你好世界"
 let 🐶🐮 = "dogcow"
-
-
+var friendWelcome = "Hello"
+print(friendWelcome)
+friendWelcome = "Bonjour"
+print(friendWelcome)
+let languageName = "Swift"
+print(languageName)
+//Error here bacause you can't change constant variable.
+//languageName = "Swift++"
+print("The current value of friendWelcome is \(friendWelcome)")
