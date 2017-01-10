264e1424a5362376c4e38e331cc3be55821ec845
marcioaleson <marcioaleson@gmail.com>
2017-01-09 19:55:50 -0500

    Constants and Variables in Swift.

---
+++ ConstantsVariables.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+//Constant
+let maximuNumberOfLoginAttempts = 10
+//Variables
+var currentLoginAttempt = 0
+var str = "Hello, playground"
+//Multiple variables
+var x = 0, y = 0, z = 0.0
