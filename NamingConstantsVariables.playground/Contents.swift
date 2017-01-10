1e1d29e60dfb9576a05acdc0773508040a19573a
marcioaleson <marcioaleson@gmail.com>
2017-01-10 02:02:03 -0300

    Naming Constants and Variables in Swift.

---
+++ NamingConstantsVariables.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+//Constat and variable names can contain almost any character
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+
+
