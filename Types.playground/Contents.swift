f8a7dd2d64a654b34bee4919c7c554f126c4b6d2
marcioaleson <marcioaleson@gmail.com>
2017-01-10 04:38:49 -0300

    Extra literals.

--- Types.playground/Contents.swift
+++ Types.playground/Contents.swift
@@ -28,3 +28,33 @@ let minDouble = DBL_MIN
 print(minDouble)
 let maxDouble = DBL_MAX
 print(maxDouble)
+//Type Safety and Type Inference
+let meannigOfLife = 42 //Swift infers you are using int
+print(meannigOfLife)
+let pi = 3.14159
+print(pi)
+let anotherPi = 3 + 0.14159//Integer + Float swift infers Double
+print(anotherPi)
+//integer literals
+let binaryinteger = 0b10001
+print(binaryinteger)
+let octalInteger = 0o21
+print(octalInteger)
+let hexadecimalInteger = 0x11
+print(hexadecimalInteger)
+//Float literals
+let decimalDouble = 12.1875
+print(decimalDouble)
+let exponentDouble = 1.21875e1
+print(exponentDouble)
+let hexadecimalDouble = 0xC.3p0
+print(hexadecimalDouble)
+//Extra literals
+let paddedDouble = 000123.456
+print(paddedDouble)
+let oneMillion = 1_000_000
+print(oneMillion)
+let justOverOneMillion = 1_000_000.000_000_1
+print(justOverOneMillion)
+
+
