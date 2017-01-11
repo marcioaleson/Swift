0e7ab680891640f6ae1b8c96d06242c8853fed86
marcioaleson <marcioaleson@gmail.com>
2017-01-11 06:31:10 -0300

    ...

--- Types.playground/Contents.swift
+++ Types.playground/Contents.swift
@@ -72,6 +72,83 @@ print(integerPI)
 typealias AudioSample = UInt16
 var maxAmplitudeFound = AudioSample.min // UInt16.min = 0
 print(maxAmplitudeFound)
+//Bool 
+let orangeAreOrange = true
+print(orangeAreOrange)
+let turniopAreDelicius = false
+print(turniopAreDelicius)
+if turniopAreDelicius {
+    print("Mmm, tasty turnips!")
+} else{
+    print("Eww, turnips are horrible!")
+}
+let fakeBool = 1
+if fakeBool == 1 {
+    print("This is a test of fake bool!")
+}
+//Tuples
+let http404Error = (404, "Not Found")
+print(http404Error)
+let (statusCode, statusMessage) = http404Error
+print("The status code is \(statusCode)")
+print("The status message is \(statusMessage)")
+let (justTheStatusCode,_) = http404Error
+print("The status code is \(justTheStatusCode)")
+print("The status code is \(http404Error.0)")
+print("The status message is \(http404Error.1)")
+let http200Status = (statusCode: 200, description: "OK")
+print("The status code is \(http200Status.statusCode)")
+print("The status message is \(http200Status.description)")
+//Optionals
+let possibleNumber = "123"
+let convertedNumber = Int(possibleNumber)
+print(convertedNumber)
+var serverResponseCode: Int? = 404
+print(serverResponseCode)
+serverResponseCode = nil
+print(serverResponseCode)
+var surveyAnswer: String?
+print(surveyAnswer)
+if convertedNumber != nil {
+    print("convertedNumber contains some integer value.")
+}
+if convertedNumber != nil {
+    print("convertedNumber has a integer value of \(convertedNumber!)")
+}
+//Optional Binding
+if let actualNumber = Int(possibleNumber){
+    print("\"\(possibleNumber)\" has an integer value of \(actualNumber)")
+}else{
+    print("\"\(possibleNumber)\" could not be converted to an integer")
+}
+if let firstNumber = Int("4"), let secondNumber = Int("42"), firstNumber < secondNumber
+&& secondNumber < 100{
+    print("\(firstNumber) < \(secondNumber) < 100")
+}
+if let firstNumber = Int("4"){
+    if let secondNumber = Int("42"){
+        if firstNumber < secondNumber && secondNumber < 100 {
+            print("\(firstNumber) < \(secondNumber) < 100")
+        }
+    }
+}
+let possibleString: String? = "An optional string!"
+print(possibleString)
+let forcedString: String = possibleString!
+print(forcedString)
+let assumedString: String! = "An implicitly unwrapped optinal string."
+print(assumedString)
+let implicitString: String = assumedString
+print(implicitString)
+if assumedString != nil {
+    print(assumedString)
+}
+if let definiteString = assumedString {
+   print(definiteString)
+}
+
+
+
 
 
 
