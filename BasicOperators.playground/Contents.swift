fe1e17fae40981a0f388e09997a93e9b56516a29
marcioaleson <marcioaleson@gmail.com>
2017-01-16 06:35:25 -0300

    Basic Operators.

---
+++ BasicOperators.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let b = 10
+var a = 5
+print(a)
+//Assignment
+a = b
+print(a)
+let (x, y) = (1, 2)
+print("The value of x=\(x) and y=\(y)")
+//Arithmetic Operators
+print("Addition \(1+1)")
+print("Subtraction \(2-1)")
+print("Multiplication \(2*2)")
+print("Division \(4/2)")
