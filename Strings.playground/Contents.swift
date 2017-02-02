b7163f283ad32e928d228d4491e0284a05e18a82
marcioaleson <marcioaleson@gmail.com>
2017-02-02 17:51:35 -0300

    Added string operation.

--- Strings.playground/Contents.swift
+++ Strings.playground/Contents.swift
@@ -4,4 +4,30 @@ import Cocoa
 
 var str = "Hello, playground"
 print(str)
+//Some string literal
+let someString = "Some string literal value"
+//Initialize empty string
+var emptyString = ""
+var anotherEmptyString = String()
+if emptyString.isEmpty{
+    print("Nothig to see here")
+}
+var variableString = "Horse"
+variableString += " and carriage"
+print(variableString)
+let constantString = "Highlander"
+//You can't added in constant Strings
+//constantString += " and another Highlander"
+//Working with characters
+for character in "Dog!🐶".characters {
+    print(character)
+}
+//Stand-alone Character
+let exclamationMark: Character = "!"
+let catCharacters: [Character] = ["C", "a", "t","!","🐱" ]
+let catString = String(catCharacters)
+print(catString)
+
+
+
 
