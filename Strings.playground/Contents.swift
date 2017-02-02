cae9e22a6a952584bc271a052f372cceaec0fd17
marcioaleson <marcioaleson@gmail.com>
2017-02-02 20:54:16 -0300

    Unicode examples.

--- Strings.playground/Contents.swift
+++ Strings.playground/Contents.swift
@@ -27,6 +27,30 @@ let exclamationMark: Character = "!"
 let catCharacters: [Character] = ["C", "a", "t","!","🐱" ]
 let catString = String(catCharacters)
 print(catString)
+let string1 = "hello"
+let string2 = " there"
+var welcome = string1 + string2
+print(welcome)
+var instruction = "look over"
+instruction += string2
+print(instruction)
+welcome.append(exclamationMark)
+print(welcome)
+let multiplier = 3
+let message = "\(multiplier) times 2.5 is \(Double(multiplier) * 2.5)"
+print(message)
+let wiseWords = "\"Imagination is more important than knowledge\" - Einstein"
+print(wiseWords)
+let dollarSign = "\u{24}"
+print(dollarSign)
+let blackHeart = "\u{2665}"
+print(blackHeart)
+let sparklingHeart = "\u{1F496}"
+print(sparklingHeart)
+
+
+
+
 
 
 
